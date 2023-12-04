6e5b79d98af26374d193c17de93c06c5bd111cf1
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-12-04 15:06:05 +0100

    research: revert setting

--- SneakerUS/settings.py
+++ SneakerUS/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/products')
 MEDIA_URL = '/products/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
